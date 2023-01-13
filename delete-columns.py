bb28e43cf6cff9d8276b14aa2d6e592c63ba47f4
yonas <115009469+yonass08@users.noreply.github.com>
2023-01-13 18:17:37 +0300

    Create delete-columns.py

---
+++ delete-columns.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        res = 0
+
+        for col in range(len(strs[0])):
+            for row in range(len(strs) - 1):
+                if strs[row][col] > strs[row+1][col]:
+                    res += 1
+                    break
+
+        return res
