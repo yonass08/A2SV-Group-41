b46cefbcef0aba99ec60d3277bfff3ac419e63ac
yonas <115009469+yonass08@users.noreply.github.com>
2023-03-08 10:20:59 +0300

    Update kth-symbol-grammar.py

--- kth-symbol-grammar.py
+++ kth-symbol-grammar.py
@@ -2,12 +2,11 @@ class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
         if k == 1:
             return 0
-        if k == 2:
-            return 1
-
-        pivot = 2 ** int(math.log2(k))
-        next_ = [[0, 1], [1, 0]]
-
-        offset = (k - pivot - 1)
-        return next_[self.kthGrammar(n, (pivot // 2) + (offset // 2) + 1)][offset % 2]
+        
+        prev = self.kthGrammar(n-1, (k+1) // 2)
+        
+        if k % 2 == 1:
+            return prev
+        else:
+            return 1 - prev
 
