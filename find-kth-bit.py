b14ca1956479bcbbf8cbe97ea489f713e2f1f697
yonas <115009469+yonass08@users.noreply.github.com>
2023-03-08 10:29:31 +0300

    Update find-kth-bit.py

--- find-kth-bit.py
+++ find-kth-bit.py
@@ -1,13 +1,12 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        mlist = ['0'] * k
-        pivot = 0
-        for i in range(1, k):
-            if i == 2 * (pivot+1) - 1:
-                mlist[i] = '1'
-                pivot = i
-            else:
-                index = 2 * pivot - i
-                mlist[i] = '1' if mlist[index] == '0' else '0'
+        if n == 1:
+            return "0"
 
-        return mlist[-1]
+        if k < 2 ** (n-1):
+            return self.findKthBit(n-1, k)
+        elif k > 2 ** (n-1):
+            return str(1 - int(self.findKthBit(n-1, (2 ** n) - k)))
+        else:
+            return '1'
+
