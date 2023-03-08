4ad1b5db87a7e19d2e683f7b095174d3f4385d88
yonas <115009469+yonass08@users.noreply.github.com>
2023-03-08 11:50:14 +0300

    Create merge-binarty-trees.py

---
+++ merge-binarty-trees.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 == None:
+            return root2
+
+        if root2 == None:
+            return root1
+
+        new_node = TreeNode(root1.val + root2.val)
+        new_node.left = self.mergeTrees(root1.left, root2.left)
+        new_node.right = self.mergeTrees(root1.right, root2.right)
+
+        return new_node
