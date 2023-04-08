b4d7f546d4b7d322819e1c0689953582bf813002
yonas <115009469+yonass08@users.noreply.github.com>
2023-04-08 19:36:30 +0300

    Create hamming-distance.py

---
+++ hamming-distance.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        res = 0
+
+        while x or y:
+            res += (x ^ y) & 1
+            x >>= 1
+            y >>= 1
+
+        return res
