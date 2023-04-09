2a5db4f8e66377db1e179a4d522d12f13a6ee4a6
yonas <115009469+yonass08@users.noreply.github.com>
2023-04-09 22:40:12 +0300

    Create max-depth-nary-tree.py

---
+++ max-depth-nary-tree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root == None:
+            return 0
+
+        res = 0
+        for child in root.children:
+            res = max(res, self.maxDepth(child))
+
+        return res+1
