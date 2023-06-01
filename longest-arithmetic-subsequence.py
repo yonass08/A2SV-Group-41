80dcc4fc7864bb41c1f3347bb30fa439ebf3561b
yonas <115009469+yonass08@users.noreply.github.com>
2023-06-01 18:40:04 +0300

    added longest-arithmetic-subsequence.py

---
+++ longest-arithmetic-subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        dp = defaultdict(int)
+
+        for num in arr:
+            dp[num] = max(dp[num], 1 + dp[num - difference])
+
+        return max(dp.values())
