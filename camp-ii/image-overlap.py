c90cd2706188ab506a475649045fc4ce17d0c211
yonas <115009469+yonass08@users.noreply.github.com>
2023-10-13 18:56:17 +0300

    Added solution for Image Overlap

--- camp-ii/image-overlap.py
+++ camp-ii/image-overlap.py
@@ -1,18 +1,24 @@
 class Solution:
     def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
+        n = len(A)
+        
+        isInbound = lambda row, col: 0 <= row < n and 0 <= col < n
 
-        import numpy as np
-        A = np.array(A)
-        B = np.array(B)
-
-        dim = len(A)
-        B_padded = np.pad(B, dim-1, mode='constant', constant_values=(0, 0))
-
-        max_overlaps = 0
-        for x_shift in range(dim*2 - 1):
-            for y_shift in range(dim* 2 - 1):
-                kernel = B_padded[x_shift:x_shift+dim, y_shift:y_shift+dim]
-                non_zeros = np.sum(A * kernel)
-                max_overlaps = max(max_overlaps, non_zeros)
-
-        return max_overlaps
+        def countOverlap(sr, sc):
+            overlaps = 0
+            
+            for row in range(n):
+                for col in range(n):
+                    if isInbound(row-sr, col-sc) and A[row-sr][col-sc] == B[row][col] == 1:
+                        overlaps += 1
+                        
+            return overlaps
+        
+        res = 0
+        
+        for sr in range(-n+1, n):
+            for sc in range(-n+1, n):
+                res = max(res, countOverlap(sr, sc))
+                
+        return res
+                    
